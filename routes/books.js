f19ba1fb40ea932feb0bf6b75d9d6aa8b6d7d255
Jesse Brink <jbrink0925@gmail.com>
2021-09-29 22:31:30 -0400

    add json schema to validate request body in API

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const Book = require("../models/book");
+const jsonschema = require("jsonschema");
+const bookSchema = require("../schemas/bookSchema.json");
+const ExpressError = require("../expressError");
 
 const router = new express.Router();
 
@@ -30,8 +33,14 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
-    const book = await Book.create(req.body);
-    return res.status(201).json({ book });
+    const result = jsonschema.validate(req.body, bookSchema);
+    if (result.valid) {
+      const book = await Book.create(req.body);
+      return res.status(201).json({ book });
+    } else {
+      const listOfErrors = result.errors.map(err => err.stack);
+      throw new ExpressError(listOfErrors, 400);
+    }
   } catch (err) {
     return next(err);
   }
@@ -41,8 +50,14 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
-    const book = await Book.update(req.params.isbn, req.body);
-    return res.json({ book });
+    const result = jsonschema.validate(req.body, bookSchema);
+    if (result.valid) {
+      const book = await Book.update(req.params.isbn, req.body);
+      return res.json({ book });
+    } else {
+      const listOfErrors = result.errors.map(err => err.stack);
+      throw new ExpressError(listOfErrors, 400);
+    }
   } catch (err) {
     return next(err);
   }
